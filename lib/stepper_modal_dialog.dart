62c168c43fea34f6c7ac03f4d2772b2c561be1e8
Félix Chenette-Stewart <felix@nexumnet.com>
2024-06-16 01:23:27 -0400

    more interactive modal

--- lib/stepper_modal_dialog.dart
+++ lib/stepper_modal_dialog.dart
@@ -14,11 +14,13 @@ class StepperModalDialog extends StatefulWidget {
 }
 
 class _StepperModalDialogState extends State<StepperModalDialog> {
-  int _currentStep = 0;
+  final PageController _pageController = PageController();
   final List<TextEditingController> _controllers = List.generate(5, (_) => TextEditingController());
+  int _currentStep = 0;
 
   void _nextStep() {
     if (_currentStep < 4) {
+      _pageController.nextPage(duration: Duration(milliseconds: 300), curve: Curves.easeInOut);
       setState(() {
         _currentStep++;
       });
@@ -27,6 +29,7 @@ class _StepperModalDialogState extends State<StepperModalDialog> {
 
   void _previousStep() {
     if (_currentStep > 0) {
+      _pageController.previousPage(duration: Duration(milliseconds: 300), curve: Curves.easeInOut);
       setState(() {
         _currentStep--;
       });
@@ -36,57 +39,193 @@ class _StepperModalDialogState extends State<StepperModalDialog> {
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
-      title: Row(
-        children: [
-          Icon(Icons.directions_walk),
-          SizedBox(width: 10),
-          Text('Step ${_currentStep + 1}', style: TextStyle(fontWeight: FontWeight.bold)),
-        ],
-      ),
       content: Container(
         width: MediaQuery.of(context).size.width * 0.2,
-        height: MediaQuery.of(context).size.height * 0.3,
+        height: MediaQuery.of(context).size.height * 0.4,
         child: Column(
-          mainAxisSize: MainAxisSize.min,
           children: [
-            Text('Enter the starting address'),
-            TextField(
-              controller: _controllers[_currentStep],
-              decoration: InputDecoration(
-                labelText: 'Address',
-                border: OutlineInputBorder(),
-                filled: true,
-                fillColor: Colors.grey[200],
+            // Title Row
+            Row(
+              children: [
+                Icon(Icons.directions_walk),
+                SizedBox(width: 10),
+                Text('Step ${_currentStep + 1}', style: TextStyle(fontWeight: FontWeight.bold)),
+              ],
+            ),
+            SizedBox(height: 20),
+
+            // Content Row
+            Expanded(
+              child: PageView(
+                controller: _pageController,
+                physics: NeverScrollableScrollPhysics(),
+                children: [
+                  _buildStep1(),
+                  _buildStep2(),
+                  _buildStep3(),
+                  _buildStep4(),
+                  _buildStep5(),
+                ],
+              ),
+            ),
+            SizedBox(height: 20),
+
+            // Navigation Row
+            Row(
+              children: [
+                // Previous Button
+                Expanded(
+                  child: Align(
+                    alignment: Alignment.center,
+                    child: _currentStep > 0
+                        ? ElevatedButton(
+                            onPressed: _previousStep,
+                            child: Text('Previous'),
+                          )
+                        : SizedBox.shrink(),
+                  ),
+                ),
+                // Generate Button
+                Expanded(
+                  child: Align(
+                    alignment: Alignment.center,
+                    child: _currentStep == 4
+                        ? ElevatedButton(
+                            onPressed: widget.onGenerate,
+                            child: Text('Generate'),
+                          )
+                        : SizedBox.shrink(),
+                  ),
+                ),
+                // Next Button
+                Expanded(
+                  child: Align(
+                    alignment: Alignment.center,
+                    child: _currentStep < 4
+                        ? ElevatedButton(
+                            onPressed: _nextStep,
+                            child: Text('Next'),
+                          )
+                        : SizedBox.shrink(),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 10),
+            Align(
+              alignment: Alignment.bottomRight,
+              child: TextButton(
+                onPressed: widget.onClose,
+                child: Text('Close', style: TextStyle(color: Colors.red)),
               ),
             ),
           ],
         ),
       ),
-      actions: [
-        if (_currentStep > 0)
-          TextButton(
-            onPressed: _previousStep,
-            child: Text('Previous'),
-          ),
-        if (_currentStep < 4)
-          TextButton(
-            onPressed: _nextStep,
-            child: Text('Next'),
-          ),
-        if (_currentStep == 4)
-          TextButton(
-            onPressed: widget.onGenerate,
-            child: Text('Generate', style: TextStyle(color: Colors.green)),
-          ),
-        TextButton(
-          onPressed: widget.onClose,
-          child: Text('Close', style: TextStyle(color: Colors.red)),
-        ),
-      ],
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.all(Radius.circular(15)),
       ),
       backgroundColor: Colors.white,
+    );
+  }
+
+  Widget _buildStep1() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(Icons.location_on, size: 100),
+        Text('Step 1: Current Address', style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+        SizedBox(height: 20),
+        TextField(
+          controller: _controllers[0],
+          decoration: InputDecoration(
+            labelText: 'Current Address',
+            border: OutlineInputBorder(),
+            filled: true,
+            fillColor: Colors.grey[200],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildStep2() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(Icons.flag, size: 100),
+        Text('Step 2: Destination Address', style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+        SizedBox(height: 20),
+        TextField(
+          controller: _controllers[1],
+          decoration: InputDecoration(
+            labelText: 'Destination Address',
+            border: OutlineInputBorder(),
+            filled: true,
+            fillColor: Colors.grey[200],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildStep3() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(Icons.access_time, size: 100),
+        Text('Step 3: Time', style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+        SizedBox(height: 20),
+        TextField(
+          controller: _controllers[2],
+          decoration: InputDecoration(
+            labelText: 'Time',
+            border: OutlineInputBorder(),
+            filled: true,
+            fillColor: Colors.grey[200],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildStep4() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(Icons.directions_car, size: 100),
+        Text('Step 4: Transportation Mode', style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+        SizedBox(height: 20),
+        TextField(
+          controller: _controllers[3],
+          decoration: InputDecoration(
+            labelText: 'Transportation Mode',
+            border: OutlineInputBorder(),
+            filled: true,
+            fillColor: Colors.grey[200],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildStep5() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(Icons.description, size: 100),
+        Text('Step 5: Summary', style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+        SizedBox(height: 20),
+        TextField(
+          controller: _controllers[4],
+          decoration: InputDecoration(
+            labelText: 'Summary',
+            border: OutlineInputBorder(),
+            filled: true,
+            fillColor: Colors.grey[200],
+          ),
+        ),
+      ],
     );
   }
 }
