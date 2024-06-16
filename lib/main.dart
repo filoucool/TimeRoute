85c7bc6e73c2ec9de218a191939128f872503fd2
Félix Chenette-Stewart <felix@nexumnet.com>
2024-06-15 20:58:18 -0400

    adjusted logo

--- lib/main.dart
+++ lib/main.dart
@@ -106,13 +106,13 @@ class _MyHomePageState extends State<MyHomePage> {
             ],
           ),
           Positioned(
-            top: 16,
+            top: -40,
             left: 0,
             right: 0,
             child: Center(
               child: SvgPicture.asset(
                 'assets/TimeRoute1.svg',
-                height: 60,
+                height: 150,
               ),
             ),
           ),
